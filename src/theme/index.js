8e568f6b5db18450e9a8fda0032bee5d4e7dbcbb
Piyush <pcatjee@gmail.com>
2023-04-30 00:06:35 +0530

    starter file

---
+++ src/theme/index.js
@@ -0,0 +1,20 @@
+const theme = {
+  colors: {
+    primary: "#F04B4D",
+  },
+  background: {
+    primary: "#343645",
+  },
+  fontFamily: {
+    regular: "SFProDisplay-Regular",
+    black: "SFProDisplay-Black",
+    medium: "SFProDisplay-Medium",
+    bold: "SFProDisplay-Bold",
+    semiBold: "SFProDisplay-Semibold",
+    thin: "SFProDisplay-Thin",
+  },
+  boldStyle: {
+    bold: "bold",
+  },
+};
+export { theme };
